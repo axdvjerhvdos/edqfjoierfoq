ed2f488477e12a38ec557f3690bd580be58b03f2
Marco Antonio Cortés Rastrollo <falcatin@gmail.com>
2014-11-28 17:38:30 +0100

    Subida de usuario , comunidad autónoma y provincia.

---
+++ data/src/main/java/com/mac/shop/model/Provincia.java
@@ -0,0 +1,38 @@
+package com.mac.shop.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (schema="macShop2015")
+public class Provincia {
+	
+	@Id
+	@Column(name="idProvincia")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false, length = 100, unique=true)
+	private String nombre;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+}
--- data/src/main/java/com/mac/shop/model/Usuario.java
+++ data/src/main/java/com/mac/shop/model/Usuario.java
@@ -1,10 +1,71 @@
 package com.mac.shop.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table (name="Usuario", schema="macShop2015")
 public class Usuario {
 
+	@Id
+	@Column(name="idUsuario")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 	
+	@Column(nullable = false, unique=true, length = 100)
+	private String email;
+	
+	@Column(nullable = false, length = 100)
+	private String pass;
+	
+	@Column(nullable = false, length = 100)
+	private String nombre;
+	
+	@Column(nullable = false, length = 100)
+	private String apellido;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
 	
 }
